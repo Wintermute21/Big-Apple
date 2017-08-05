8239d7c72e18969abb884ab108c45ee13ee62ffb
John Best <jbest145@icloud.com>
2017-08-05 18:25:48 -0400

    Basic MVC project

---
+++ Big-Apple/Big-Apple/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Big_Apple.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
